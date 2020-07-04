c2648c86737a43f4568f6878d6e0ebe5c350d496
dastasoft <dastasoft@gmail.com>
2020-07-04 19:22:15 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './styles/app.css';
 
 ReactDOM.render(
   <React.StrictMode>
